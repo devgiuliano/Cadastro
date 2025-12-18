82edc2945e6fd6b4f296c017a22da0782f370af6
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2025-12-18 10:31:35 -0300

    feat: atualiza o controller com o mock de dados

--- src/main/java/dev/pessoalprojects/cadastro/Pessoas/PessoaController.java
+++ src/main/java/dev/pessoalprojects/cadastro/Pessoas/PessoaController.java
@@ -1,9 +1,7 @@
 package dev.pessoalprojects.cadastro.Pessoas;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,30 @@ public class PessoaController {
     @GetMapping("boasvindas")
     public String boasVindas(){
         return "Livia liviazinha is my love ... S2";
+    }
+    // Adicionar pessoa(CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Pessoa criada";
+    }
+    // Mostrar pessoa por id(READ)
+    @GetMapping("/todosID")
+    public String mostrarPessoaPorId(){
+        return "Mostrar pessoas por id";
+    }
+    // Mostrar todas as pessoas(READ)
+    @GetMapping("/todos")
+    public String mostrarTodasAsPessoas(){
+        return "Mostrar todas as pessoas";
+    }
+    // Alterar dados da pessoa(UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarPessoaPorId(){
+        return "Alterar pessoa por id";
+    }
+    // Deletar pessoa(DELETE)
+    @DeleteMapping("/deletarPorID")
+    public String deletarPessoaPorId(){
+        return "Pessoa deletada por id";
     }
 }
