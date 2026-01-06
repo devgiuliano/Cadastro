b679335e7a43493588af15cff00a4894153c68e5
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2026-01-06 11:29:08 -0300

    injeta dependencia do service e cria as rotas para listar e listar por id

--- src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasController.java
+++ src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasController.java
@@ -2,22 +2,30 @@ package dev.pessoalprojects.cadastro.Tarefas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("tarefas")
 public class TarefasController {
+    private TarefasService tarefasService;
+
+    public TarefasController(TarefasService tarefasService) {
+        this.tarefasService = tarefasService;
+    }
+
     @PostMapping("/criar")
     public String criarTarefa() {
         return "Tarefa criada com sucesso";
     }
 
     @GetMapping("/listar")
-    public String mostrarTarefa() {
-        return "Tarefas listadas com sucesso";
+    public List<TarefasModel> mostrarTarefa(){
+        return tarefasService.mostrarTarefa();
     }
 
-    @GetMapping("/listarID")
-    public String mostrarPorId() {
-        return "Mostrar tarefa por id";
+    @GetMapping("/listar/{id}")
+    public TarefasModel listarTarefaPorId(@PathVariable Long id) {
+        return tarefasService.listarTarefaPorId(id);
     }
 
     @PutMapping("/alterarID")
