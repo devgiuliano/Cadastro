c86fa59bfd6f111e778c1d965c47e8e132e6d66f
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2026-01-23 15:01:09 -0300

    chore: inclui a versão do DTO para entidade é mapeia manualmente no pessoaMapper os caminhos entidadexDTO

--- src/main/java/dev/pessoalprojects/cadastro/Pessoas/PessoaDTO.java
+++ src/main/java/dev/pessoalprojects/cadastro/Pessoas/PessoaDTO.java
@@ -1,7 +1,12 @@
 package dev.pessoalprojects.cadastro.Pessoas;
 import dev.pessoalprojects.cadastro.Tarefas.TarefasModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaDTO {
     private Long id;
     private String nome;
