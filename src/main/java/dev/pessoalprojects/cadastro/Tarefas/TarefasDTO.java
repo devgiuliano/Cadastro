5d63336645795bcaedd6c8022c03a0fadb909b96
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2026-01-25 11:47:32 -0300

    feat: inclui DTO e Mapper a tarefas

--- src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasDTO.java
+++ src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasDTO.java
@@ -1,4 +1,19 @@
 package dev.pessoalprojects.cadastro.Tarefas;
 
+import dev.pessoalprojects.cadastro.Pessoas.PessoaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TarefasDTO {
+
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private List<PessoaModel> pessoas;
+
 }
--- src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasMapper.java
+++ src/main/java/dev/pessoalprojects/cadastro/Tarefas/TarefasMapper.java
@@ -1,4 +1,25 @@
 package dev.pessoalprojects.cadastro.Tarefas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TarefasMapper {
+    public TarefasModel map(TarefasDTO tarefasDTO){
+        TarefasModel tarefasModel = new TarefasModel();
+        tarefasModel.setId(tarefasDTO.getId());
+        tarefasModel.setNome(tarefasDTO.getNome());
+        tarefasModel.setDificuldade(tarefasDTO.getDificuldade());
+        tarefasModel.setPessoas(tarefasDTO.getPessoas());
+
+        return tarefasModel;
+    }
+    public TarefasDTO map(TarefasModel tarefasModel){
+        TarefasDTO tarefasDTO = new TarefasDTO();
+        tarefasDTO.setId(tarefasModel.getId());
+        tarefasDTO.setNome(tarefasModel.getNome());
+        tarefasDTO.setDificuldade(tarefasModel.getDificuldade());
+        tarefasDTO.setPessoas(tarefasModel.getPessoas());
+
+        return tarefasDTO;
+    }
 }
